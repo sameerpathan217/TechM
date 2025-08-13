cceba4f8196b841adf57083d15ee47f4ef164806
sameerpathan217 <131335998+sameerpathan217@users.noreply.github.com>
2025-08-13 22:47:15 +0530

    Update CreateDocuments.java

--- src/test/java/VtigerPackage001/CreateDocuments.java
+++ src/test/java/VtigerPackage001/CreateDocuments.java
@@ -21,7 +21,7 @@ public class CreateDocuments extends BaseClass{
 		
 		System.out.println("push the latest code to githhub");
 		System.out.println("Hi new branch");
-		System.out.println("Hih");
+		System.out.println("Hih123456789");
 
 	}
 
